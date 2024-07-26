6e4fc26b4b0e218c0e5654518b4c52a8ccca46fc
Dale Lane <dale.lane@uk.ibm.com>
2024-07-26 13:11:28 +0100

    update

--- cleanup.sh
+++ cleanup.sh
@@ -13,10 +13,10 @@ for space in "${allspaces[@]}"
 do
     echo "resetting namespace $STUDENT_USERNAME-$space"
 
-    oc delete deployment -n $STUDENT_USERNAME-$space consumer-$space --ignore-not-found
+    oc delete deployment --wait=true -n $STUDENT_USERNAME-$space consumer-$space --ignore-not-found
 
     for thing in "${allthings[@]}"
     do
-        oc delete -n $STUDENT_USERNAME-$space `oc get -n $STUDENT_USERNAME-$space -o name $thing`
+        oc delete -n $STUDENT_USERNAME-$space --wait=true `oc get -n $STUDENT_USERNAME-$space -o name $thing`
     done
 done
